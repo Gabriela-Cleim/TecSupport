7d060fae48bc268f7cf21707339f1b112d80ad29
Gabriela Cleim <71110646+Gabriela-Cleim@users.noreply.github.com>
2021-10-27 16:17:39 -0300

    Add files via upload

---
+++ TecSupport/src/main/java/View/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import Model.Servico;
+
+/**
+ *
+ * @author gabri
+ */
+public class Main {
+    
+    public static void main (String[] args){
+        
+        String nome = "tiago";
+        System.out.println(nome);
+        
+        Servico formatacao = new Servico (1, "Formatação", 40);
+        
+        System.out.println(formatacao.getDescricao());
+    
+        
+        
+    
+    
+    }
+    
+}
